235dbd3cb8de08c53f2716d3a6496abe05009324
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-01 12:34:59 +0600

    added loadphone function

---
+++ js/phone.js
@@ -0,0 +1,7 @@
+const loadPhone = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await response.json();
+    console.log(data.data);
+}
+
+loadPhone();
