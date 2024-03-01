6047f17be5a7e0e882dad308744a28688fc8981a
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-03-02 01:07:46 +0600

    added modal to show phone details

--- js/phone.js
+++ js/phone.js
@@ -17,7 +17,7 @@ const displayPhone = (phones, isShowAll) => {
     showAllBtn.classList.add("hidden");
   }
 
-  if(!isShowAll){
+  if (!isShowAll) {
     phones = phones.slice(0, 9);
   }
 
@@ -32,7 +32,7 @@ const displayPhone = (phones, isShowAll) => {
        <h2 class="card-title">${phone.phone_name}</h2>
        <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions">
-          <button class="btn bg-[#0D6EFD] text-white">Show Details</button>
+          <button onclick="showPhoneDetailsHandler('${phone.slug}')" class="btn bg-[#0D6EFD] text-white">Show Details</button>
         </div>
      </div>`;
     cardContainer.appendChild(div);
@@ -47,23 +47,49 @@ const searchHandler = (isShowAll) => {
 
   loadPhone(searchText, isShowAll);
 
-  if(isShowAll){
+  if (isShowAll) {
     searchPhoneField.value = "";
   }
 };
 
 const loadSpinner = (isLoading) => {
   const loadingSpinner = document.getElementById("loadingSpinner");
-  if(isLoading){
+  if (isLoading) {
     loadingSpinner.classList.remove("hidden");
-  }
-  else{
+  } else {
     loadingSpinner.classList.add("hidden");
   }
 };
 
 const showAllHandler = () => {
-    searchHandler(true);
-}
+  searchHandler(true);
+};
 
+const showPhoneDetailsHandler = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+
+  showPhoneDetails(data.data);
+};
+
+const showPhoneDetails = (phoneDetails) => {
+    const phoneModelName = document.getElementById('phoneModelName');
+    phoneModelName.innerText = phoneDetails.name;
+
+    const phoneDetailsContainer = document.getElementById('phoneDetailsContainer');
+    phoneDetailsContainer.innerHTML = `
+    <img src="${phoneDetails?.image}" alt="">
+    <h3><span class="font-semibold text-xl">Storage: </span>${phoneDetails?.mainFeatures?.storage}</h3>
+    <h3><span class="font-semibold text-xl">Display Size: </span>${phoneDetails?.mainFeatures?.displaySize}</h3>
+    <h3><span class="font-semibold text-xl">Chipset: </span>${phoneDetails?.mainFeatures?.chipSet}</h3>
+    <h3><span class="font-semibold text-xl">Memory: </span>${phoneDetails?.mainFeatures?.memory}</h3>
+    <h3><span class="font-semibold text-xl">Slug: </span>${phoneDetails?.slug}</h3>
+    <h3><span class="font-semibold text-xl">Release Date: </span>${phoneDetails?.releaseDate}</h3>
+    <h3><span class="font-semibold text-xl">Brand: </span>${phoneDetails.brand}</h3>
+    <h3><span class="font-semibold text-xl">GPS: </span>${phoneDetails?.others?.GPS}</h3>
+    `
+    my_modal_5.showModal();
+};
 // loadPhone();
